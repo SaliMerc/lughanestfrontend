47f56ef4332b297700d607398af987bb5c0b64d7
SaliMerc <nyawadesaline@gmail.com>
2025-08-29 12:33:06 +0300

    fixes'

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [tailwindcss(),
-//     react()
-//   ],
-//   base: process.env.VITE_BASE_PATH || "/"
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(), // This should work if you have the correct package
+  plugins: [tailwindcss(),
     react()
   ],
-  base: process.env.VITE_BASE_PATH || "/",
-  build: {
-    outDir: '../dist', 
-    assetsDir: 'assets',
-    manifest: true, 
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
-  }
+  base: process.env.VITE_BASE_PATH || "/"
 })
+
+
 
 
